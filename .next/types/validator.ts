c5d8888e03d272d4e6e689e7ae388cc5770238da
Maddie H.(Work) <99705111+mahadiatwork@users.noreply.github.com>
2026-01-22 06:08:36 +0800

    added test route

--- .next/types/validator.ts
+++ .next/types/validator.ts
@@ -92,6 +92,15 @@ type RouteHandlerConfig<Route extends AppRouteHandlerRoutes = AppRouteHandlerRou
   type __Unused = __Check
 }
 
+// Validate ../../src/app/(main)/entry/test_new/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/entry/test_new">> = Specific
+  const handler = {} as typeof import("../../src/app/(main)/entry/test_new/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../src/app/(main)/history/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/history">> = Specific
